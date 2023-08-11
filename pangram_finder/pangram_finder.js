94653ee12a0e1e1288d9c0d3eebbebe03a189aa1
John <ashton.basile@gmail.com>
2023-08-11 09:55:28 +0100

    I solved the pangram_finder exercise

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,8 +1,13 @@
 class PangramFinder {
   constructor(phrase) {
     this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+    this.phrase = phrase.toLowerCase();
   }
+
   isPangram() {
+    return this.alphabet.every((letter) => {
+      return this.phrase.includes(letter);
+    });
   }
 }
 
