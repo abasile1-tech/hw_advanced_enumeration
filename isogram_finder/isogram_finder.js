033e06f8eda20ca04c5435a697683e4cd71bc32c
John <ashton.basile@gmail.com>
2023-08-11 10:09:01 +0100

    I solved the isogram finder exercise

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,7 +1,16 @@
 class IsogramFinder {
 	constructor(word) {
+		this.word = word.toLowerCase().split('');
 	}
+
 	isIsogram() {
+		return this.word.every((letter) => {
+			return this.isUnique(letter);
+		});
+	}
+
+	isUnique(letter) {
+		return this.word.indexOf(letter) === this.word.lastIndexOf(letter);
 	}
 }
 
